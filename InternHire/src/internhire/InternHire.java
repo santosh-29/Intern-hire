8dca06ebf8fe84b4107b51de8bfc8cb945e1e25f
unknown <santoshkumarsoma6@gmail.com>
2022-01-25 19:53:08 +0530

    Add existing file

---
+++ InternHire/src/internhire/InternHire.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package internhire;
+
+/**
+ *
+ * @author INDIA
+ */
+public class InternHire {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //RUN LOADINGSCREEN.java TO RUN THE PROJECT
+//        LoadingScreen ls = new LoadingScreen();
+//        ls.setVisible(true);
+    }
+    
+}
